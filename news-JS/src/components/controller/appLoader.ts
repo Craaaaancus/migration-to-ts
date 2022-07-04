439079420cb2e9551d5f396340bcc9afb087668a
Denis Greckij <denisgreckij38@gmail.com>
2022-07-05 00:45:54 +0300

    fix: change url

--- news-JS/src/components/controller/appLoader.ts
+++ news-JS/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './Loader/loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
+    super('https://nodenews.herokuapp.com/', {
       apiKey: 'caa85f7c9dae4dd8ab7a91d09783bdfa', // получите свой ключ https://newsapi.org/
     });
   }
