ba6e4ed9933e195f5f37e95f400aa53a162e5e1f
Denis Greckij <denisgreckij38@gmail.com>
2022-07-04 19:20:56 +0300

    feat: make sources-block sticky

--- news-JS/src/components/view/sources/sources.ts
+++ news-JS/src/components/view/sources/sources.ts
@@ -21,7 +21,7 @@ class Sources {
       fragment.append(sourceClone);
     });
 
-    document.querySelector('.sources')!.append(fragment);
+    document.querySelector('.source__list')!.append(fragment);
   }
 }
 
